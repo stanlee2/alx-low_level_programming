084b14ae6ed7b8bf728545135e2cc6d14b8c89cc
stanlee2 <nzubechistanley2@gmail.com>
2023-03-05 17:38:34 -0800

    C language;4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with put function
+ * return: 0(done)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
